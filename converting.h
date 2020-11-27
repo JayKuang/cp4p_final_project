24f721b8dc2a3050acb6e7b29e0708dc0da2065b
Jincheng Kuang <jkuang10@myseneca.ca>
2020-11-26 22:18:17 -0500

    converting.c

---
+++ converting.h
@@ -0,0 +1,12 @@
+//CONVERTING V1 - commentted by Jincheng Kuang
+
+#ifndef _CONVERTING_H // if not define _CONVERTING_H
+#define _CONVERTING_H // then define it _CONVERTING_H
+
+#include <stdio.h> // include header file stdio.h
+#include <string.h> // include header file string.h
+#include <stdlib.h> // include header file stdlib.h
+#define TRUE 1 // define a global variable TRUE as 1
+void converting(); // declare the converting function prototype
+
+#endif // close the ifndef
